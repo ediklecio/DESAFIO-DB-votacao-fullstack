c447a859a499615380772bd591b75ae229cb010d
Ediklecio <ediklecio.developer@gmail.com>
2026-09-20 15:47:50 -0300

    feat: Add Spring Boot template

---
+++ src/main/java/com/db/vote/VoteApplication.java
@@ -0,0 +1,13 @@
+package com.db.vote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VoteApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(VoteApplication.class, args);
+	}
+
+}
